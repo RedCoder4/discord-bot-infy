8aac4602608a094fa4b61b0359c8dda76411da24
Adnayerc <33248319+Adnayerc@users.noreply.github.com>
2017-10-31 09:32:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,10 @@
+console.log('Estoy listo para servir al clan :D!');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('Estoy listo!');
+bot.on('message', (message) => {
+	if (message.content == 'hola'){
+		message.channel.sendMessage('Hola! :D');
+	}
 });
-
-bot.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-bot.login(process.env.Mzc0NjAxNTQ3MzQ4MTgwOTky.DNm_cg.UwrimGeUzAT4ip9T7Lh1vChFC5s);
+bot.login('Mzc0NjAxNTQ3MzQ4MTgwOTky.DNm_cg.UwrimGeUzAT4ip9T7Lh1vChFC5s');
