11a1471ca0f40c52f9cd30f3cd1081ecfe7cf442
Adnayerc <33248319+Adnayerc@users.noreply.github.com>
2017-10-31 09:10:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,15 @@
-console.log('El bot esta listo para INFY');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on(?message', (message) => {
-	if (message.content == 'hola'){
-		message.channel.sendMessage({message: "Hello World", tts: true});
+bot.on('ready', () => {
+    console.log('Estoy listo!');
 });
-bot.login('Mzc0NjAxNTQ3MzQ4MTgwOTky.DNm_cg.UwrimGeUzAT4ip9T7Lh1vChFC5s');
+
+bot.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+bot.login(process.env.Mzc0NjAxNTQ3MzQ4MTgwOTky.DNm_cg.UwrimGeUzAT4ip9T7Lh1vChFC5s);
