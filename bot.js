71f2eae1e51ea8be32e7ba3a39af09b9d9611694
Adnayerc <33248319+Adnayerc@users.noreply.github.com>
2017-10-31 15:37:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,11 @@ bot.on('message' , (message) => {
 			message.channel.send("Horarios Scrim: De Viernes a Domingos (De 4 a 8)", {
 			tts: true
 			})
+	}
 	if (message.content == '!mikel') {
 			message.channel.send("@TheMaik96#1746, MID Laner del equipo: Este chico es pura skill con una goodpull bien grande. Solo el lag le para!", {
 			tts:true
 			})
+	}
 });
 bot.login('Mzc0NjAxNTQ3MzQ4MTgwOTky.DNjqZg.azMs_Syyoq4tf0mz40maSHK87fU');
