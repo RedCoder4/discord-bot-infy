6190eaa11570a9dbea189813ab834e8f560864d0
Adnayerc <33248319+Adnayerc@users.noreply.github.com>
2017-10-31 17:38:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,27 @@ const bot = new Discord.Client();
 bot.on('message' , (message) => {
 	if (message.content == '!scrim') {
 			message.channel.send("Horarios Scrim: De Viernes a Domingos (De 4 a 8)", {
-			tts: true
+			tts:false
 			})
 	}
 	if (message.content == '!mikel') {
-			message.channel.send("@TheMaik96, MID Laner del equipo: Este chico es pura skill con una goodpull bien grande. Solo el lag le para!", {
-			tts:true
+			message.channel.send("TheMaik96, mid laner del equipo: Este chico es pura skill con una goodpull bien grande. Solo el lag le para!", {
+			tts:false
+			})
+	}
+	if (message.content == '!rivas') {
+			message.channel.send("Rivasito, solo laner del equipo: Mejor jugador, streamer y de todo lo que puedas imaginar de EU y pronto mundial!", {
+			tts:false
+			})
+	}
+	if (message.content == '!adnayerc') {
+			message.channel.send("Adnayerc, support del equipo: Amante del bodyblock, gran defensor de este rol hasta la muerte!!", {
+			tts:false
+			})
+	}
+	if (message.content == '!roster') {
+			message.channel.send("Roster actual(IDs):\n-MID: TheMaik96\n-ADC: LF\n-Jungle: LF\n-Support: Adnáyerc\n-Solo: Rivasito\n-Suplentes: LF", {
+			tts: false
 			})
 	}
 });
